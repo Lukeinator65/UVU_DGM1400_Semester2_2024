b8e92f6940146dc70da2f3b4b809417f237adb48
Lukeinator65 <161548894+Lukeinator65@users.noreply.github.com>
2024-04-12 09:08:23 -0600

    Created Operators and Variables files

---
+++ Basics/001 Conventions and Syntax/Variables.cs
@@ -0,0 +1,13 @@
+public class Variables {
+
+    public void Main()
+    {
+        hamBurger = newFood
+    }
+}
+
+public class Food {
+    public int pickleCount = 4;
+    public string foodName = "Hamburger";
+    
+}
