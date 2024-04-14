5156bc92468188fc79e7e04a894e17cbbddffccb
Lukeinator65 <161548894+Lukeinator65@users.noreply.github.com>
2024-04-14 13:57:52 -0600

    Updates to 1a challenge

--- Basics/001 Conventions and Syntax/Unit1aChallenge.cs
+++ Basics/001 Conventions and Syntax/Unit1aChallenge.cs
@@ -2,5 +2,18 @@ using System;
 
 public class Unit1AChallenge
 {
-    
+    public void Main(string[] args)
+    {
+		Console.WriteLine("Enter your name:");
+		string name = Console.ReadLine();
+		
+        Console.WriteLine("Enter your age:");
+        int age = Convert.ToInt32(Console.ReadLine());
+		
+		Console.WriteLine("Enter an amount of money including cents:");
+		float money = Convert.ToFloat(Console.ReadLine());
+       
+		Console.WriteLine("Your name is " + name);
+		Console.WriteLine("and you are " + age + " years old.");
+    }
 }
