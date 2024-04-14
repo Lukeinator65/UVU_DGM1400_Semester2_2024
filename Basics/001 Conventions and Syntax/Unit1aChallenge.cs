93b84999116ede2d70e9991bdace469da0e7ed2f
Lukeinator65 <161548894+Lukeinator65@users.noreply.github.com>
2024-04-14 14:13:37 -0600

    More changes to Unit1a challenge

--- Basics/001 Conventions and Syntax/Unit1aChallenge.cs
+++ Basics/001 Conventions and Syntax/Unit1aChallenge.cs
@@ -2,6 +2,9 @@ using System;
 
 public class Unit1AChallenge
 {
+    public Years someDecades;
+    public Income someIncome;
+
     public void Main(string[] args)
     {
 		Console.WriteLine("Enter your name:");
@@ -16,5 +19,20 @@ public class Unit1AChallenge
 		Console.WriteLine("Your name is " + name);
 		Console.WriteLine("and you are " + age + " years old.");
 		Console.WriteLine("You have $" + money + " in your bank account.");
+
+        someDecades = new Years();
+        someIncome = new Income();
     }
+}
+
+public class Years {
+    public int twoDecades = 20;
+    public int fiveDecades = 50;
+    public int tenDecades = 100;
+}
+
+public class Income{
+	public float incomeTwoDecades = 2.0f;
+	public float incomeFiveDecades = 5.0f;
+	public float incomeTenDecades = 10.0f;
 }
