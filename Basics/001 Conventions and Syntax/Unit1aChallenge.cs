237a3b4cd915d75042c5c0cf547c32bc7d46aa70
Lukeinator65 <161548894+Lukeinator65@users.noreply.github.com>
2024-04-14 14:03:15 -0600

    Oh yea, I figured out user input for floats

--- Basics/001 Conventions and Syntax/Unit1aChallenge.cs
+++ Basics/001 Conventions and Syntax/Unit1aChallenge.cs
@@ -11,9 +11,10 @@ public class Unit1AChallenge
         int age = Convert.ToInt32(Console.ReadLine());
 		
 		Console.WriteLine("Enter an amount of money including cents:");
-		float money = Convert.ToFloat(Console.ReadLine());
+		float money = float.Parse(Console.ReadLine());
        
 		Console.WriteLine("Your name is " + name);
 		Console.WriteLine("and you are " + age + " years old.");
+		Console.WriteLine("You have $" + money + " in your bank account.");
     }
 }
