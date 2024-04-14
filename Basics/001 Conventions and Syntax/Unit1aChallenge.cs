a047ebe63576dd9f81ba07314be68925ebe09026
Lukeinator65 <161548894+Lukeinator65@users.noreply.github.com>
2024-04-14 14:41:34 -0600

    Finialized Unit1a Challenge

--- Basics/001 Conventions and Syntax/Unit1aChallenge.cs
+++ Basics/001 Conventions and Syntax/Unit1aChallenge.cs
@@ -3,36 +3,61 @@ using System;
 public class Unit1AChallenge
 {
     public Years someDecades;
-    public Income someIncome;
+    public Income multIncome;
 
     public void Main(string[] args)
     {
-		Console.WriteLine("Enter your name:");
+		someDecades = new Years();
+		multIncome = new Income();
+		
+		Console.WriteLine("Welcome to Decade Income Multiplier!");
+		Console.WriteLine("");
+		Console.WriteLine("For each decade, you money will be multiplied by that amount of decades!");
+		Console.WriteLine("");
+		
+		Console.WriteLine("Please enter your name:");
 		string name = Console.ReadLine();
 		
-        Console.WriteLine("Enter your age:");
+        Console.WriteLine("Please enter your age:");
         int age = Convert.ToInt32(Console.ReadLine());
 		
 		Console.WriteLine("Enter an amount of money including cents:");
 		float money = float.Parse(Console.ReadLine());
-       
+		
+		Console.WriteLine("");
 		Console.WriteLine("Your name is " + name);
 		Console.WriteLine("and you are " + age + " years old.");
+		Console.WriteLine("");
 		Console.WriteLine("You have $" + money + " in your bank account.");
-
-        someDecades = new Years();
-        someIncome = new Income();
+		
+		money *= multIncome.incomeTwoDecades;
+		age += someDecades.twoDecades;
+		Console.WriteLine("");
+		Console.WriteLine("In two decades, you will be " + age + " years old.");
+		Console.WriteLine("and your money will multiply up to $" + money); 
+		
+		money *= multIncome.incomeFiveDecades;
+		age += someDecades.fiveDecades;
+		Console.WriteLine("");
+		Console.WriteLine("In three more decades, you will be " + age + " years old.");
+		Console.WriteLine("and your money will multiply up to $" + money); 
+		
+		money *= multIncome.incomeTenDecades;
+		age += someDecades.tenDecades;
+		Console.WriteLine("");
+		Console.WriteLine("Finally, in five more decades, you will be " + age + " years old.");
+		Console.WriteLine("and your money will multiply up to $" + money); 
     }
 }
 
 public class Years {
     public int twoDecades = 20;
-    public int fiveDecades = 50;
-    public int tenDecades = 100;
+    public int fiveDecades = 30;
+    public int tenDecades = 50;
 }
 
-public class Income{
+public class Income {
 	public float incomeTwoDecades = 2.0f;
-	public float incomeFiveDecades = 5.0f;
-	public float incomeTenDecades = 10.0f;
+	public float incomeFiveDecades = 3.0f;
+	public float incomeTenDecades = 5.0f;
 }
