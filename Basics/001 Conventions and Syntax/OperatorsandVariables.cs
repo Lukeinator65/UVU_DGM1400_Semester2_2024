acb0dbee663bbe56c7caa2b30e4366c0ca5af28c
Lukeinator65 <161548894+Lukeinator65@users.noreply.github.com>
2024-04-14 12:26:15 -0600

    WIP Lab file

--- Basics/001 Conventions and Syntax/OperatorsandVariables.cs
+++ Basics/001 Conventions and Syntax/OperatorsandVariables.cs
@@ -1,13 +1,23 @@
-public class Variables {
+public class Variables 
+{
+    public Food hamBurger;
+    public Side 
 
     public void Main()
     {
-        hamBurger = newFood
+        hamBurger = new Food();
+        frenchFry = new Side();
     }
 }
 
 public class Food {
     public int pickleCount = 4;
     public string foodName = "Hamburger";
-    
+    public float burgerPrice = 9.99f;
+}
+
+public class Side {
+    public int fryCount = 25;
+    public string foodName = "French Fries";
+    public float fryPrice = 2.99f;
 }
