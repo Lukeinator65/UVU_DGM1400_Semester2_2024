1fc22a21d009e8008009a9d148017532bcbb3d77
Lukeinator65 <161548894+Lukeinator65@users.noreply.github.com>
2024-04-14 15:04:01 -0600

    Notes added to Unit1aChallenge

--- Basics/001 Conventions and Syntax/Unit1aChallenge.cs
+++ Basics/001 Conventions and Syntax/Unit1aChallenge.cs
@@ -10,11 +10,11 @@ public class Unit1AChallenge
 		someDecades = new Years();
 		multIncome = new Income();
 		
-		Console.WriteLine("Welcome to Decade Income Multiplier!");
+		Console.WriteLine("Welcome to Decade Income Multiplier!"); //Introduces program name and purpose
 		Console.WriteLine("");
 		Console.WriteLine("For each decade, your money will be multiplied by that amount of decades!");
 		Console.WriteLine("");
-		
+		//Allows for user input for multiple variables
 		Console.WriteLine("Please enter your name:");
 		string name = Console.ReadLine();
 		
@@ -23,13 +23,13 @@ public class Unit1AChallenge
 		
 		Console.WriteLine("Enter an amount of money including cents:");
 		float money = float.Parse(Console.ReadLine());
-		
+		//Repeats back user input
 		Console.WriteLine("");
 		Console.WriteLine("Your name is " + name);
 		Console.WriteLine("and you are " + age + " years old.");
 		Console.WriteLine("");
 		Console.WriteLine("You have $" + money + " in your bank account.");
-		
+		//Variable manipulation with multiplication of float variables and addition of integer variables
 		money *= multIncome.incomeTwoDecades;
 		age += someDecades.twoDecades;
 		Console.WriteLine("");
@@ -49,13 +49,13 @@ public class Unit1AChallenge
 		Console.WriteLine("and your money will multiply up to $" + money); 
     }
 }
-
+//Determines interger varibles for the amount of decades
 public class Years {
     public int twoDecades = 20;
     public int fiveDecades = 30;
     public int tenDecades = 50;
 }
-
+//Determines float variables for the money multiplier
 public class Income {
 	public float incomeTwoDecades = 2.0f;
 	public float incomeFiveDecades = 3.0f;
