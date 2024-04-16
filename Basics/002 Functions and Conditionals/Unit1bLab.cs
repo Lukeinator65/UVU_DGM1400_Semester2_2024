b6edf09de28be7b19e1c5a2b0dbde73e96e791fd
Lukeinator65 <161548894+Lukeinator65@users.noreply.github.com>
2024-04-16 12:48:26 -0600

    Added notes explaining code in Unit1bLab

--- Basics/002 Functions and Conditionals/Unit1bLab.cs
+++ Basics/002 Functions and Conditionals/Unit1bLab.cs
@@ -8,21 +8,21 @@ public class Unit1bLab
 	{
 		tempInCelcius = new Temperature();
 		
-		Console.WriteLine("Enter the current temperature in Celcius:");
-		int temp = Convert.ToInt32(Console.ReadLine());
-		tempInCelcius.CheckTemp(temp);
+		Console.WriteLine("Enter the current temperature in Celcius:"); //Ask for user input
+		int temp = Convert.ToInt32(Console.ReadLine()); //Takes user input and defines the temp
+		tempInCelcius.CheckTemp(temp); //Runs Temperature Check Operation
     }
 }
 
 public class Temperature {
 	public void CheckTemp (int temp){
-		if(temp > 30) {
+		if(temp > 30) { //If user input temp is greater than 30, it will tell user to stay hydrated
 		Console.WriteLine("");
 		Console.WriteLine("It's " + temp + "° celcius outside!" + " Make sure to stay hyrdated!");
-	} else if(temp >= 20) {
+	} else if(temp >= 20) { //If user input temp is greater than or equal to 20, it will tell user that its nice out
 			Console.WriteLine("");
 			Console.WriteLine("It's " + temp + "° celcius outside!" + " Its's very nice out!");
-		} else {
+		} else { //If user input temp is anything else, it will tell user to wear a jacket
 			Console.WriteLine("");
 			Console.WriteLine("It's " + temp + "° celcius outside!" + " Make sure to wear a jacket!");
 		
