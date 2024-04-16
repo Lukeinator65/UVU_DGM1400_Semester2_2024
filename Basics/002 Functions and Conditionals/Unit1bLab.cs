0116b04b8f19ac3ed95dd8e7035ebac4f3699cb9
Lukeinator65 <161548894+Lukeinator65@users.noreply.github.com>
2024-04-16 12:19:51 -0600

    Unit1b Lab Temp Check Done

---
+++ Basics/002 Functions and Conditionals/Unit1bLab.cs
@@ -0,0 +1,31 @@
+using System;
+					
+public class Unit1bLab
+{
+	public Temperature tempInCelcius;
+	
+	public void Main(string[] args)
+	{
+		tempInCelcius = new Temperature();
+		
+		Console.WriteLine("Enter the current temperature in Celcius:");
+		int temp = Convert.ToInt32(Console.ReadLine());
+		tempInCelcius.CheckTemp(temp);
+    }
+}
+
+public class Temperature {
+	public void CheckTemp (int temp){
+		if(temp > 30) {
+		Console.WriteLine("");
+		Console.WriteLine("It's " + temp + " degrees celcius outside!" + " Make sure to stay hyrdated!");
+	} else if(temp == 20) {
+			Console.WriteLine("");
+			Console.WriteLine("It's " + temp + " degrees celcius outside!" + " Its's very nice out!");
+		} else {
+			Console.WriteLine("");
+			Console.WriteLine("It's " + temp + " degrees celcius outside!" + " Make sure to wear a jacket!");
+		
+		}
+	}
+}
