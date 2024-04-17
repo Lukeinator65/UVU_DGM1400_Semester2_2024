991329db2338a8a7fe98324b51bcf44bce453d99
Lukeinator65 <161548894+Lukeinator65@users.noreply.github.com>
2024-04-16 18:41:48 -0600

    Still workin on it

--- Basics/002 Functions and Conditionals/Unit1bChallenge.cs
+++ Basics/002 Functions and Conditionals/Unit1bChallenge.cs
@@ -1,17 +1,15 @@
 using System;
-					
+
 public class Unit1bChallenge
 {
 	public Temperature tempInCelcius;
 	public Grader examGrader;
 	public SchoolSubject favSubject;
-	
 	public void Main(string[] args)
 	{
 		tempInCelcius = new Temperature();
 		examGrader = new Grader();
 		favSubject = new SchoolSubject();
-		
 		Console.WriteLine("Enter the current temperature in Celcius:");
 		int temp = Convert.ToInt32(Console.ReadLine()); //Takes user input and defines the temp
 		Console.WriteLine("Enter an exam grade:");
@@ -21,53 +19,77 @@ public class Unit1bChallenge
 		tempInCelcius.CheckTemp(temp); //Runs Temperature Check Operation
 		examGrader.CheckGrade(grade); //Runs Grade Check Operation
 		favSubject.CheckSubject();
-    }
+	}
 }
 
-public class Temperature {
-	public void CheckTemp (int temp){
-		if(temp > 40) { //If user input temp is greater than 30, it will tell user to stay hydrated
-		Console.WriteLine("");
-		Console.WriteLine("It's " + temp + "° celcius outside!" + " That's a bit too hot. May sure to stay inside, drink and stay cool!");
-		} else if(40 > temp && temp >= 30) { //If user input temp is less than 30 and greater than or equal to 20, it will tell user that its nice out
+public class Temperature
+{
+	public void CheckTemp(int temp)
+	{
+		if (temp > 40)
+		{ //If user input temp is greater than 30, it will tell user to stay hydrated
+			Console.WriteLine("");
+			Console.WriteLine("It's " + temp + "° celcius outside!" + " That's a bit too hot. May sure to stay inside, drink and stay cool!");
+		}
+		else if (40 > temp && temp >= 30)
+		{ //If user input temp is less than 30 and greater than or equal to 20, it will tell user that its nice out
 			Console.WriteLine("");
 			Console.WriteLine("It's " + temp + "° celcius outside!" + " That's pretty hot, so make sure to wear short clothes and to stay hydrated!");
-		} else if(30 > temp && temp >= 18) {
+		}
+		else if (30 > temp && temp >= 18)
+		{
 			Console.WriteLine("");
 			Console.WriteLine("It's " + temp + "° celcius outside!" + " That's pretty warm. You may not need to wear short clothes, but you should still stay hydrated!");
-		} else if(18 > temp && temp >= 5) {
+		}
+		else if (18 > temp && temp >= 5)
+		{
 			Console.WriteLine("");
 			Console.WriteLine("It's " + temp + "° celcius outside!" + " That's a bit cool, so you might want to wear a light jacket.");
-		} else { //If user input temp is anything else, it will tell user to wear a jacket
+		}
+		else
+		{ //If user input temp is anything else, it will tell user to wear a jacket
 			Console.WriteLine("");
 			Console.WriteLine("It's " + temp + "° celcius outside!" + " That's pretty cold! You might want to wear a heavy jacket!");
 		}
 	}
 }
 
-public class Grader {
-	public void CheckGrade (int grade){
-		if (grade >= 90) { //If user input grade is greater than or equal to 90, it will tell user they have an A
-		Console.WriteLine("");
-		Console.WriteLine("You got a " + grade + " on your exam!" + " That's an A!");
-		} else if(90 > grade && grade >= 80) { //If user input grade is less than 90 and is greater than or equal to 80, it will tell user they have a B
+public class Grader
+{
+	public void CheckGrade(int grade)
+	{
+		if (grade >= 90)
+		{ //If user input grade is greater than or equal to 90, it will tell user they have an A
+			Console.WriteLine("");
+			Console.WriteLine("You got a " + grade + " on your exam!" + " That's an A!");
+		}
+		else if (90 > grade && grade >= 80)
+		{ //If user input grade is less than 90 and is greater than or equal to 80, it will tell user they have a B
 			Console.WriteLine("");
 			Console.WriteLine("You got a " + grade + " on your exam!" + " That's a B!");
-		} else if(80 > grade && grade >= 70) { //If user input grade is less than 80 and is greater than or equal to 70, it will tell user they have an C
+		}
+		else if (80 > grade && grade >= 70)
+		{ //If user input grade is less than 80 and is greater than or equal to 70, it will tell user they have an C
 			Console.WriteLine("");
 			Console.WriteLine("You got a " + grade + " on your exam." + " That's a C.");
-		} else if(70 > grade && grade >= 60) { //If user input grade is less than 70 and is greater than or equal to 60, it will tell user they have an D
+		}
+		else if (70 > grade && grade >= 60)
+		{ //If user input grade is less than 70 and is greater than or equal to 60, it will tell user they have an D
 			Console.WriteLine("");
 			Console.WriteLine("You got a " + grade + " on your exam." + " That's a D.");
-		} else { //If user input is less than 60, it will tell user they have an F
+		}
+		else
+		{ //If user input is less than 60, it will tell user they have an F
 			Console.WriteLine("");
 			Console.WriteLine("Oh, that's not good. You got a " + grade + " on your exam." + " That's a F.");
 		}
 	}
 }
 
-public class SchoolSubject {
-	public enum Subjects {
+public class SchoolSubject
+{
+	public enum Subjects
+	{
 		English,
 		Math,
 		History,
@@ -75,11 +97,12 @@ public class SchoolSubject {
 		Art,
 		PE
 	}
-	
-	public Subjects favSubject = Subjects.English;
 
-	public void CheckSubject () {
-		switch (favSubject){
+	public Subjects favSubject = (subject);
+	public void CheckSubject()
+	{
+		switch (favSubject)
+		{
 			case Subjects.English:
 				Console.WriteLine("");
 				Console.WriteLine("Your favorite subject is English! Make sure to check your grammar and spelling!");
@@ -101,6 +124,5 @@ public class SchoolSubject {
 				Console.WriteLine("Your favorite subject is Art! Make sure to put your heart into your work!");
 				break;
 		}
-
 	}
 }
