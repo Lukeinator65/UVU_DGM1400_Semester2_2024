2afae53f8896df9e0efef9e695a79203e21edff9
Luke <lukeinator59@gmail.com>
2024-04-30 10:00:28 -0600

    Finished 2.1

--- Unity Projects/CreateWithCode/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Unity Projects/CreateWithCode/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,15 +14,17 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    public float horizontalInput;
-    public float speed = 10.0f;
     void Update()
     {
+        // Keep the player in bounds
+        if (transform.position.x < -xRange){
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        } 
+
+        if (transform.position.x > xRange){
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        } 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-
-        if (transform.position.x < -10){
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
-        }  
     }
 }
