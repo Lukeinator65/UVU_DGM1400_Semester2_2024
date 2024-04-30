caf41e6cdc71d695f8d56784964567e7e7eefcff
Luke <lukeinator59@gmail.com>
2024-04-30 12:28:24 -0600

    More 2.2 progress

--- Unity Projects/CreateWithCode/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unity Projects/CreateWithCode/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
+
 {
     // Start is called before the first frame update
     void Start()
@@ -11,8 +12,11 @@ public class DestroyOutOfBounds : MonoBehaviour
     }
 
     // Update is called once per frame
+    private float topBound = 30;
     void Update()
     {
-        
+        if(transform.position.z > topBound){
+            Destroy(gameObject);
+        }
     }
 }
