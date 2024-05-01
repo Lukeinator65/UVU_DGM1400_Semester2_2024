52594488fde61d1dca40580845ea26d07ae5a1f0
Luke <lukeinator59@gmail.com>
2024-05-01 15:21:40 -0600

    Finished Lab 4c

--- Unity Projects/CreateWithCode/Personal Project/Assets/Scripts/PlayerController.cs
+++ Unity Projects/CreateWithCode/Personal Project/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float horizontalInput;
+    private float speed = 15.0f;
+    private float xRange = 10;
+    private Rigidbody playerRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+    void MovePlayer()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);       
+    }
+
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        } 
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
     }
 }
