7fb61ed5f805fdd0b2f15c0e28b5076b3b1217e7
Luke <lukeinator59@gmail.com>
2024-05-01 10:17:17 -0600

    Working on 2.4 and adjusted speeds

--- Unity Projects/CreateWithCode/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Unity Projects/CreateWithCode/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 10.0f;
-    public float xRange = 10;
+    public float speed = 12.5f;
+    public float xRange = 17;
 
     public GameObject projectilePrefab;
 
