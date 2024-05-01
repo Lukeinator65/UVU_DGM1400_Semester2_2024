301e13f25622c24e9a01f8cf3f1ec52c5b2bfee8
Luke <lukeinator59@gmail.com>
2024-05-01 09:45:48 -0600

    Progress on 2.3

--- Unity Projects/CreateWithCode/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Unity Projects/CreateWithCode/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -12,12 +12,17 @@ public class SpawnManager : MonoBehaviour
 
     // Update is called once per frame
     public GameObject[]animalPrefabs;
-    public int animalIndex;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
 
     void Update()
     {
        if (Input.GetKeyDown(KeyCode.S)){
-        Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
+        //Randomly generate animal index and spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 
+        0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos,
         animalPrefabs[animalIndex].transform.rotation);
        } 
     }
