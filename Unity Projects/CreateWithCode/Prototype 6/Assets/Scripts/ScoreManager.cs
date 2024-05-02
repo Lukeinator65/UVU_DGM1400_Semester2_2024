64a7b50816185bb0d34c37822ed2f567556e8bd3
Luke <lukeinator59@gmail.com>
2024-05-02 06:59:19 -0600

    Started 6c2

---
+++ Unity Projects/CreateWithCode/Prototype 6/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // Text Mesh Pro name spaced added to access the librarys for UI
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score; // Keep our score value
+    public TextMeshProUGUI scoreText; // Visual text element to be modified
+
+    public void IncreaseScore(int amount) // This method when called increases the score by a predetermined amount set in score variable
+    {
+        score += amount;
+        UpdateScoreText();
+
+    }
+
+    public void DecreaseScore(int amount) // This method when called decreases the score by a predetermined amount set in score variable
+    {
+        score -= amount
+        UpdateScoreText();
+    }
+
+    public void UpdateScoreText()
+    {
+        scoreText = "Score: "+ score;
+    }
+}
