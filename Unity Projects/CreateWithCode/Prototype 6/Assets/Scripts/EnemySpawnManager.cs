7603ec327be150690c57888dd80275b056b5fd78
Luke <lukeinator59@gmail.com>
2024-05-02 05:38:27 -0600

    Finished 6b2

--- Unity Projects/CreateWithCode/Prototype 6/Assets/Scripts/EnemySpawnManager.cs
+++ Unity Projects/CreateWithCode/Prototype 6/Assets/Scripts/EnemySpawnManager.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class EnemySpawnManager : MonoBehaviour
 {
     public GameObject[] ufoPrefabs; //Array to store UFO ships
-    public int ufoIndex;
+    private float spawnRangeX = 20f;
+    private float spawnPosZ = 20f;
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.S))
         {
-            Instantiate(ufoPrefabs[ufoIndex], new Vector3(0, 0, 15), ufoPrefabs[ufoIndex].transform.rotation);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int ufoIndex = Random.Range(0, ufoPrefabs.Length); // Picks a random UFO from the array
+            Instantiate(ufoPrefabs[ufoIndex], spawnPos, ufoPrefabs[ufoIndex].transform.rotation); // Spawns a indexed UFO from the array at a random location on the X-Axis
         }
     }
 }
