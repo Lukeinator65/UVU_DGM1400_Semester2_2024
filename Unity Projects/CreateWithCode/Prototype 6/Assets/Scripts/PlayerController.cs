b6648a3d51cbb824859e3951306a86a2d9c7545a
Luke <lukeinator59@gmail.com>
2024-05-02 03:08:24 -0600

    Finished 6a2

--- Unity Projects/CreateWithCode/Prototype 6/Assets/Scripts/PlayerController.cs
+++ Unity Projects/CreateWithCode/Prototype 6/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float speed = 25;
+    public float xRange = 30;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    // Set HorizontalInput to recieve values from keyboard
+        horizontalInput = Input.GetAxis("Horizontal");
+
+    // Moves player left and right
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    // Keep player within bounds
+    // Left Side Wall
+        if(transform.position.x < -xRange )
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+    // Right Side Wall
+        if(transform.position.x > xRange )
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
     }
 }
